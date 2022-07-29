d30a3db03bfcdd0bd893fc49e609b1173df2a4d5
Jeh-nishiyama <jessicanishiyama@gmail.com>
2022-07-29 12:19:24 -0300

    add arq javascript

---
+++ introducao ao javascript/TESTE/teste.js
@@ -0,0 +1,16 @@
+//comentário de uma linha
+
+/* comentário
+de várias
+linhas */
+
+// constante sempre colocar em letra maiúscula
+
+//função precisa chamar novamente a operação que você deseja depois da função, se não dá erro no código
+function soma(a, b) {
+    return a + b;
+}
+
+soma(3, 5);
+
+console.log("Hello World!");
